9d5af3ace319cfcd59cde340c5d741c3f6e4afb4
andysarbini <kangandy09@gmail.com>
2021-06-30 21:15:11 +0700

    endpoint categories

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,189 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+      v-if="isHome"
+    >
+    
+      <!-- <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn> -->
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title to="/">{{ appName }}</v-toolbar-title>
+
+      <!-- pemisah konten -->
+      <v-spacer></v-spacer>
+
+      <v-btn icon to="/about">
+        <v-badge color="orange overlap">
+          <template v-slot:badge>
+            <span>3</span>
+          </template>
+        </v-badge>
+        <v-icon>mdi-cart</v-icon>
+      </v-btn>
+
+      <v-text-field slot="extension"
+      hide-details
+      append-icon="mdi-microphone"
+      flat
+      label="Search"
+      prepend-inner-icon="mdi-magnify"
+      solo-inverted>
+      </v-text-field>
+    
+    </v-app-bar>
+
+    <v-app-bar
+      app
+      color="primary"
+      dark
+      v-else
+    >
+      <v-btn icon @click.stop="$router.go(-1)">
+        <v-icon>mdi-arrow-left-circle</v-icon>
+
+      </v-btn>
+      <v-spacer></v-spacer>
+      <v-btn icon to="/about">
+        <v-badge color="orange" overlap>
+          <template v-slot:badge>
+            <span>3</span>
+          </template>
+          <v-icon>mdi-cart</v-icon>
+        </v-badge>
+      </v-btn>
+    </v-app-bar>
+
+    <!-- <v-main>
+      <router-view/>
+    </v-main> -->
+    <v-card>
+      <v-navigation-drawer app v-model="drawer">
+        <v-list>
+          <v-list-item v-if="!guest">
+            <v-list-item-avatar>
+              <v-img src="https://randomuser.me/api/portraits/men/78.jpg"></v-img>
+            </v-list-item-avatar>
+            
+            <v-list-item-content>
+              <v-list-item-title>John Leider</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+
+            <div class="pa-2" v-if="guest">
+              <v-btn block color="primary" class="mb-1">
+                <v-icon left>mdi-lock</v-icon>
+                Login
+              </v-btn>
+              <v-btn block color="success">
+                <v-icon left>mdi-account</v-icon>
+                Register
+              </v-btn>              
+            </div>
+          <v-divider></v-divider>
+
+          <v-list-item
+          v-for="(item, index) in menus"
+          :key = "`menu-` +index"
+          :to = "item.route">
+            <v-list-item-icon>
+              <v-icon left>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+
+        </v-list>
+
+        <template v-slot:append v-if="!guest">
+        <div class="pa-2">
+            <v-btn block color="red" dark>
+              <v-icon left>mdi-lock</v-icon>
+              Logout
+            </v-btn>
+          </div>  
+        </template>
+
+      </v-navigation-drawer>      
+    </v-card>
+
+
+    <v-main>
+      <v-container fluid>
+        <v-slide-y-transition>
+          <!-- jika menggunakan vue-router -->
+          <router-view></router-view>
+        </v-slide-y-transition>
+
+      </v-container>
+    </v-main>
+
+          
+
+
+    <!-- <v-footer app>
+      FOOTER
+    </v-footer> -->
+    <v-card>
+      <v-footer absolute app>
+        <v-card-text class="text-center">
+          &copy; {{ new Date().getFullYear() }} - <strong>Vueshop</strong>
+        </v-card-text>
+      </v-footer>
+    </v-card>
+  </v-app>
 </template>
 
-<style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
-</style>
+<script>
+  export default {
+    name: 'App',  
+    data: () => ({
+      drawer: false,
+      menus: [
+        { title: 'Home', icon: 'mdi-home', route: '/'},
+        { title: 'About', icon: 'mdi-account', route: '/about'},
+      ],
+      guest: false,
+      computed: {
+        isHome() {
+          return (this.$route.path==='/')
+        },
+      }
+
+  })
+  };
+
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,120 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <hello-world /> -->
+  <div>
+    <!-- template categories -->
+    <v-container class="ma-0 pa-0" grid-list-sm>
+      <div class="text-right">
+        <v-btn small text to="/categories" class="blue--text">
+          All Categories <v-icon>mdi-chevron-right</v-icon>
+        </v-btn>
+      </div>
+      <v-layout wrap>
+        <v-flex v-for="(category, index) in categories" :key="`category-
+        `+category.id" xs6>
+          <v-card :to="'/category/'+ category.slug">
+            <v-img
+              :src="getImage('/categories/'+category.image)"
+              class="white--text">
+
+              {{ index+1 }}
+              <v-card-title
+              class="fill-height align-end"
+              v-text="category.name">
+              </v-card-title>
+              
+
+            </v-img>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+
+    <v-container class="ma-0 pa-0 mt-2" grid-list-sm>
+            <div class="text-right">
+              <v-btn small text to="/books" class="blue--text">
+                All Books <v-icon>mdi-chevron-right</v-icon>
+              </v-btn>
+            </div>
+            <v-layout wrap>
+              <v-flex v-for="(book) in books" :key="`book-`+book.id" xs6>
+                <v-card :to="'/book/'+ book.slug">
+                  <v-img
+                    :src="getImage('/books/'+book.cover)"
+                    class="white--text">
+                    <v-card-title
+                      class="fill-height align-end"
+                      v-text="book.title">
+                    </v-card-title>
+                  </v-img>
+                </v-card>
+              </v-flex>
+            </v-layout>
+          </v-container>
+
+    <!-- template books -->
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  // import HelloWorld from '../components/HelloWorld'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  export default {
+    // name: 'Home',
+
+    // components: {
+    //   HelloWorld,
+    // },
+    data: () => ({
+      categories: [],
+
+      books: [
+        
+      ]
+    }),
+
+created() {
+  console.log('get data categories')
+  // this.axios.get('http://localhost/larashop-api/public/api/v1/categories/random/2')
+  this.axios.get('/categories/random/2')
+    // this.axios.get('/')
+    .then((response) => {
+      let {data} = response.data
+      // console.log(data)
+      this.categories = data
+    })
+    .catch((error) => {
+      let {response} = error
+      console.log(response)
+    })
+
+    this.axios.get('/books/top/4')
+      .then((response) => {
+        let {data} = response.data
+        this.books = data
+      })
+      .catch((error) => {
+        let { response } = error
+        console.log(response)
+      })
+},
+
+// tidak perlu karena sudah ada method global di helper
+// methods: {
+//   getImage(image) {
+//     if (image!=null && image.length>0) {
+//       // return "http://localhost/larashop-api/public/images"+ image
+//       return process.env.VUE_APP_BACKEND_URL +"/images"+ image
+//     }
+//     // default image jika tidak ditemukan,
+//     // letakkan image ini pada folder /public/img di project vue
+//     return "unavalilable.png"
+//   },
+// }, 
+
+mounted() {
+  console.log(process.env)
 }
+  };
 </script>
+
